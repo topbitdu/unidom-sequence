c947c4aa6ed4397c95341cc54a4431f66cd5e91d
Topbit Du <topbit.du@gmail.com>
2016-12-20 11:49:23 +0800

    1, Improve the Sequence model to add the .generate! method.

--- app/models/unidom/sequence/sequence.rb
+++ app/models/unidom/sequence/sequence.rb
@@ -8,4 +8,10 @@ class Unidom::Sequence::Sequence < Unidom::Sequence::ApplicationRecord
 
   scope :prefix_is, ->(prefix) { where prefix: prefix }
 
+  def self.generate!(sequentiality_code, prefix = '')
+    sequence = prefix_is(prefix).sequentiality_coded_as(sequentiality_code).valid_at.alive.first_or_create!
+    sequence.increment! :serial_number
+    sequence.serial_number
+  end
+
 end
