9c2c26d89a170625d0f9cf20aed6429ef2e26b22
Topbit Du <topbit.du@gmail.com>
2017-02-16 09:19:21 +0800

    1， Improve the Sequence spec for the prefix_is scope.

--- lib/rspec/models/unidom/sequence/sequence_spec.rb
+++ lib/rspec/models/unidom/sequence/sequence_spec.rb
@@ -15,6 +15,14 @@ describe Unidom::Sequence::Sequence, type: :model do
 
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
 
+    it_behaves_like 'scope', :prefix_is, [
+      { attributes_collection: [ model_attributes ], count_diff: 0, args: [ '110115' ] },
+      { attributes_collection: [ model_attributes ], count_diff: 0, args: [ 110115   ] },
+      { attributes_collection: [ model_attributes ], count_diff: 1, args: [ '110116' ] },
+      { attributes_collection: [ model_attributes ], count_diff: 1, args: [ 110116   ] },
+      { attributes_collection: [ model_attributes ], count_diff: 0, args: [ '110117' ] },
+      { attributes_collection: [ model_attributes ], count_diff: 0, args: [ 110117   ] } ]
+
   end
 
 end
