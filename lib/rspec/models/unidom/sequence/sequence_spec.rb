8d8d585c3bed861c63c9fb0e8e227e867793fd59
Topbit Du <topbit.du@gmail.com>
2017-02-25 10:24:08 +0800

    1, Improve the Sequence spec for the .generate! method.

--- lib/rspec/models/unidom/sequence/sequence_spec.rb
+++ lib/rspec/models/unidom/sequence/sequence_spec.rb
@@ -1,9 +1,46 @@
 describe Unidom::Sequence::Sequence, type: :model do
 
+  subject :sequentiality_code do 'ODNO'           end
+  subject :prefix             do 'ORDER-20171225' end
+
   before :each do
   end
 
   after :each do
+  end
+
+  it 'should be able to generate 1 new serial number' do
+
+    serial_number = described_class.generate! sequentiality_code, prefix
+    expect(serial_number).to eq(1)
+
+  end
+
+  it 'should be able to generate 2 new serial numbers' do
+
+    serial_number_1 = described_class.generate! sequentiality_code, prefix
+    expect(serial_number_1).to eq(1)
+
+    serial_number_2 = described_class.generate! sequentiality_code, prefix
+    expect(serial_number_2).to eq(2)
+
+  end
+
+  it 'should be able to generate 1 new serial number with empty prefix' do
+
+    serial_number = described_class.generate! sequentiality_code
+    expect(serial_number).to eq(1)
+
+  end
+
+  it 'should be able to generate 2 new serial numbers with empty prefix' do
+
+    serial_number_1 = described_class.generate! sequentiality_code
+    expect(serial_number_1).to eq(1)
+
+    serial_number_2 = described_class.generate! sequentiality_code
+    expect(serial_number_2).to eq(2)
+
   end
 
   context do
