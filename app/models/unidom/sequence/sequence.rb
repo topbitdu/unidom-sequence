a2990028ae68d50282e98b80f849deb89dc1354b
Topbit Du <topbit.du@gmail.com>
2017-04-13 07:17:05 +0800

    1, Improve the Sequence model for the document.

--- app/models/unidom/sequence/sequence.rb
+++ app/models/unidom/sequence/sequence.rb
@@ -9,6 +9,11 @@ class Unidom::Sequence::Sequence < Unidom::Sequence::ApplicationRecord
 
   scope :prefix_is, ->(prefix) { where prefix: prefix }
 
+  ##
+  # 生成下一个序列号。如：
+  # Unidom::Sequence::Sequence.generate! 'ODNO', '20200101'
+  # # or
+  # Unidom::Sequence::Sequence.generate! 'ODNO'
   def self.generate!(sequentiality_code, prefix = '')
     sequence = prefix_is(prefix).sequentiality_coded_as(sequentiality_code).valid_at.alive.first_or_create!
     sequence.increment! :serial_number
